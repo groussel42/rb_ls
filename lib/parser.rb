7aebf205643c1c76ceecbd45e5304a6ae04f613e
groussel42 <gaelroussel@paranoid.email>
2019-12-26 15:21:07 +0100

    [CHANGE] use of metaprogramming

--- lib/parser.rb
+++ lib/parser.rb
@@ -50,35 +50,9 @@ class Parser
     end
   end
 
-  def arg_l?
-    @args[:l]
-  end
-
-  def arg_R?
-    @args[:R]
-  end
-
-  def arg_a?
-    @args[:a]
-  end
-
-  def arg_r?
-    @args[:r]
-  end
-
-  def arg_t?
-    @args[:t]
-  end
-
-  def arg_S?
-    @args[:S]
-  end
-
-  def arg_U?
-    @args[:U]
-  end
-
-  def arg_x?
-    @args[:x]
+  %w[l R a r t S U x].each do |method|
+    define_method "arg_#{method}?" do
+      @args[method.to_sym]
+    end
   end
 end
